c2fe68fc34fa73bef9645e7a258c8a9c4a7dc5f9
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2021-01-04 11:47:07 -0600

    initial set up

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Link } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "Header">
+      </div>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
